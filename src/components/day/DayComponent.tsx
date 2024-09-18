3c8948200ec5e352427e5d82cf7e044dc8324809
Adjel <adjel.touaoula@gmail.com>
2024-09-18 20:29:57 +0200

    exceed day

--- src/components/day/DayComponent.tsx
+++ src/components/day/DayComponent.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { getDateFromDays } from '../../utils';
+
 
 export interface Day {
   date: Date,
@@ -11,62 +10,55 @@ export interface Slot {
   end?: Date
 }
 
-function generateRandomTime(startTime: Date): Date {
-  const startHours = startTime.getHours();
-  const startMinutes = startTime.getMinutes();
+function getRandomHour(startDate: Date): Date {
 
-  const randomDate = new Date(startTime);
+  console.log(startDate)
 
-  const hours = Math.floor(Math.random() * (24 - startHours) + startHours);
-  const minutes = Math.random() < 0.5 ? 0 : 30;
+  const endofDay = new Date(startDate)
+  endofDay.setHours(23, 59, 0, 0)
 
-  randomDate.setHours(hours, minutes, 0, 0);
+  const randomTime = Math.random() * (endofDay.getHours() - startDate.getHours()) + endofDay.getHours();
 
-  if (hours === startHours && minutes < startMinutes) {
-    generateRandomTime(startTime)
-  }
+  console.log(randomTime)
 
-  return randomDate;
+  const newTime = new Date(startDate)
+  newTime.setHours(randomTime)
+
+  return newTime
 }
 
-const startTime = new Date();
-startTime.setHours(14, 0); 
-const randomTime = generateRandomTime(startTime);
-console.log(randomTime.toTimeString());
+
+function generateRandomSlots(date: Date): Slot[] {
+  const numSlots = Math.floor(Math.random() * 11);
+  const slots: Slot[] = [];
+
+  let start = new Date(date.setHours(0, 0, 0, 0))
+  let end = getRandomHour(start)
+
+  for (let i = 0; i < numSlots && end.getHours() < 23.30; i++) {
+
+    start = end
+    end = getRandomHour(start)
+
+    slots.push({ start, end });
+  
+  }
+
+  return slots;
+}
 
 
-export function getDay(date: Date, days:number = 0): Day {
+export function getDay(date: Date, days: number = 0): Day {
   const newDate = new Date(date);
   newDate.setDate(newDate.getDate() + days);
 
-  const slots = []
-  let start
-  let end
 
-  const randomNumber = Math.floor(Math.random() * 10);
-  for (let i = 0; i < randomNumber; i++) {
-    const midnightDate = new Date(date);
-    midnightDate.setHours(0, 0, 0, 0);
-    end
-    start = generateRandomTime(end ?? midnightDate)
-    end = generateRandomTime(start)
 
-    slots.push({
-      start: start,
-      end: end
-    })
-  }
+  const slots = generateRandomSlots(date);
 
   return {
-    date: getDateFromDays(date, days),
-    slots: slots
-  }
+    date: newDate,
+    slots
+  };
 }
 
-function DayComponent() {
-  const [day, setDay]= useState<Day>()
-
-  return <div>{}</div>;
-}
-
-export default DayComponent;
